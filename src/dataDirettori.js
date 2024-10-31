b63fac5fc09c7f1e02d209d8d73018883d00b0f3
tekzoz <marco.comba.mc@gmail.com>
2024-11-01 00:33:54 +0100

    Update dataDirettori.js

--- src/dataDirettori.js
+++ src/dataDirettori.js
@@ -190,7 +190,632 @@ export const direttoriData = [
         "nome": "CAPUTO ARIANNA",
         "mese": "2024-09",
         "turni": 20
-      }    
+      },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-01",
+          "turni": 11
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-02",
+          "turni": 14
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-03",
+          "turni": 3
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-04",
+          "turni": 9
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-05",
+          "turni": 26
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-06",
+          "turni": 32
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-07",
+          "turni": 17
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-08",
+          "turni": 2
+        },
+        {
+          "nome": "CASTRACANE BARBARA",
+          "mese": "2024-09",
+          "turni": 10
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-01",
+          "turni": 4
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-02",
+          "turni": 26
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-03",
+          "turni": 29
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-04",
+          "turni": 32
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-05",
+          "turni": 42
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-06",
+          "turni": 26
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-07",
+          "turni": 41
+        },
+        {
+          "nome": "CATANIA GIULIA",
+          "mese": "2024-09",
+          "turni": 41
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-01",
+          "turni": 33
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-02",
+          "turni": 6
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-03",
+          "turni": 13
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-04",
+          "turni": 10
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-05",
+          "turni": 26
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-06",
+          "turni": 23
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-08",
+          "turni": 3
+        },
+        {
+          "nome": "CATANIA MYRIAM",
+          "mese": "2024-09",
+          "turni": 10
+        },
+        {
+          "nome": "CIGLIANO ALESSIO",
+          "mese": "2024-02",
+          "turni": 7
+        },
+        {
+          "nome": "CIGLIANO ALESSIO",
+          "mese": "2024-03",
+          "turni": 13
+        },
+        {
+          "nome": "CIGLIANO ALESSIO",
+          "mese": "2024-04",
+          "turni": 4
+        },
+        {
+          "nome": "CIGLIANO ALESSIO",
+          "mese": "2024-05",
+          "turni": 7
+        },
+        {
+          "nome": "CIGLIANO ALESSIO",
+          "mese": "2024-06",
+          "turni": 23
+        },
+        {
+          "nome": "CIGLIANO ALESSIO",
+          "mese": "2024-07",
+          "turni": 1
+        },
+        {
+          "nome": "COLTORTI JESUS EMILIANO",
+          "mese": "2024-01",
+          "turni": 1
+        },
+        {
+          "nome": "COLTORTI JESUS EMILIANO",
+          "mese": "2024-03",
+          "turni": 2
+        },
+        {
+          "nome": "COLTORTI JESUS EMILIANO",
+          "mese": "2024-04",
+          "turni": 9
+        },
+        {
+          "nome": "COLTORTI JESUS EMILIANO",
+          "mese": "2024-05",
+          "turni": 9
+        },
+        {
+          "nome": "COLTORTI JESUS EMILIANO",
+          "mese": "2024-06",
+          "turni": 3
+        },
+        {
+          "nome": "D'ANDREA SIMONE",
+          "mese": "2024-04",
+          "turni": 1
+        },
+        {
+          "nome": "DE ANGELIS ELEONORA",
+          "mese": "2024-02",
+          "turni": 14
+        },
+        {
+          "nome": "DE ANGELIS ELEONORA",
+          "mese": "2024-03",
+          "turni": 16
+        },
+        {
+          "nome": "DE ANGELIS ELEONORA",
+          "mese": "2024-04",
+          "turni": 14
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-01",
+          "turni": 31
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-02",
+          "turni": 10
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-03",
+          "turni": 7
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-04",
+          "turni": 25
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-05",
+          "turni": 36
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-06",
+          "turni": 50
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-07",
+          "turni": 35
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-08",
+          "turni": 4
+        },
+        {
+          "nome": "DE FLAVIIS FABRIZIO",
+          "mese": "2024-09",
+          "turni": 9
+        },
+        {
+          "nome": "DE FLAVIIS FLAVIO",
+          "mese": "2024-02",
+          "turni": 2
+        },
+        {
+          "nome": "DE FLAVIIS FLAVIO",
+          "mese": "2024-03",
+          "turni": 3
+        },
+        {
+          "nome": "DE FLAVIIS FLAVIO",
+          "mese": "2024-04",
+          "turni": 22
+        },
+        {
+          "nome": "DE FLAVIIS FLAVIO",
+          "mese": "2024-06",
+          "turni": 2
+        },
+        {
+          "nome": "DONATI GEMMA",
+          "mese": "2024-06",
+          "turni": 8
+        },
+        {
+          "nome": "BOLOGNESI GAIA VENUSTA",
+          "mese": "2024-06",
+          "turni": 2
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-01",
+          "turni": 17
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-02",
+          "turni": 31
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-03",
+          "turni": 20
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-04",
+          "turni": 34
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-05",
+          "turni": 23
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-06",
+          "turni": 27
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-07",
+          "turni": 30
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-08",
+          "turni": 2
+        },
+        {
+          "nome": "IZZO GIUSEPPINA",
+          "mese": "2024-09",
+          "turni": 22
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-01",
+          "turni": 18
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-02",
+          "turni": 29
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-03",
+          "turni": 11
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-04",
+          "turni": 56
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-05",
+          "turni": 59
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-06",
+          "turni": 30
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-07",
+          "turni": 55
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-08",
+          "turni": 23
+        },
+        {
+          "nome": "IZZO ROSSELLA",
+          "mese": "2024-09",
+          "turni": 20
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-01",
+          "turni": 21
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-02",
+          "turni": 19
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-04",
+          "turni": 2
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-05",
+          "turni": 8
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-06",
+          "turni": 6
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-07",
+          "turni": 4
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-08",
+          "turni": 2
+        },
+        {
+          "nome": "IZZO FIAMMETTA",
+          "mese": "2024-09",
+          "turni": 38
+        },
+        {
+          "nome": "LATINI ILARIA",
+          "mese": "2024-03",
+          "turni": 3
+        },
+        {
+          "nome": "LATINI ILARIA",
+          "mese": "2024-04",
+          "turni": 3
+        },
+        {
+          "nome": "LATINI ILARIA",
+          "mese": "2024-05",
+          "turni": 10
+        },
+        {
+          "nome": "LATINI ILARIA",
+          "mese": "2024-06",
+          "turni": 33
+        },
+        {
+          "nome": "LATINI ILARIA",
+          "mese": "2024-07",
+          "turni": 8
+        },
+        {
+          "nome": "METE ANDREA",
+          "mese": "2024-03",
+          "turni": 2
+        },
+        {
+          "nome": "METE ANDREA",
+          "mese": "2024-04",
+          "turni": 4
+        },
+        {
+          "nome": "METE ANDREA",
+          "mese": "2024-05",
+          "turni": 17
+        },
+        {
+          "nome": "METE ANDREA",
+          "mese": "2024-06",
+          "turni": 3
+        },
+        {
+          "nome": "PEZZULLI FRANCESCO",
+          "mese": "2024-06",
+          "turni": 6
+        },
+        {
+          "nome": "ROMA MANUELE",
+          "mese": "2024-04",
+          "turni": 4
+        },
+        {
+          "nome": "ROMA MANUELE",
+          "mese": "2024-05",
+          "turni": 2
+        },
+        {
+          "nome": "ROMA MANUELE",
+          "mese": "2024-06",
+          "turni": 2
+        },
+        {
+          "nome": "ROMANO LAURA",
+          "mese": "2024-03",
+          "turni": 1
+        },
+        {
+          "nome": "ROMANO LAURA",
+          "mese": "2024-04",
+          "turni": 1
+        },
+        {
+          "nome": "ROMANO LAURA",
+          "mese": "2024-05",
+          "turni": 2
+        },
+        {
+          "nome": "ROMANO LAURA",
+          "mese": "2024-06",
+          "turni": 1
+        },
+        {
+          "nome": "SCOTTI GIULIA SONIA",
+          "mese": "2024-03",
+          "turni": 5
+        },
+        {
+          "nome": "SCOTTI GIULIA SONIA",
+          "mese": "2024-04",
+          "turni": 6
+        },
+        {
+          "nome": "SCOTTI GIULIA SONIA",
+          "mese": "2024-05",
+          "turni": 2
+        },
+        {
+          "nome": "SCOTTI GIULIA SONIA",
+          "mese": "2024-06",
+          "turni": 1
+        },
+        {
+          "nome": "VAIRANO FRANCESCO",
+          "mese": "2024-04",
+          "turni": 9
+        },
+        {
+          "nome": "VAIRANO FRANCESCO",
+          "mese": "2024-05",
+          "turni": 14
+        },
+        {
+          "nome": "VAIRANO FRANCESCO",
+          "mese": "2024-06",
+          "turni": 34
+        },
+        {
+          "nome": "VAIRANO FRANCESCO",
+          "mese": "2024-07",
+          "turni": 34
+        },
+        {
+          "nome": "VAIRANO FRANCESCO",
+          "mese": "2024-08",
+          "turni": 12
+        },
+        {
+          "nome": "VAIRANO FRANCESCO",
+          "mese": "2024-09",
+          "turni": 1
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-01",
+          "turni": 6
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-02",
+          "turni": 1
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-03",
+          "turni": 14
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-04",
+          "turni": 30
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-05",
+          "turni": 19
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-06",
+          "turni": 1
+        },
+        {
+          "nome": "VENDITTI FRANCESCO SAVERIO",
+          "mese": "2024-07",
+          "turni": 4
+        },
+        {
+          "nome": "VERSINO LUDOVICO",
+          "mese": "2024-02",
+          "turni": 2
+        },
+        {
+          "nome": "VERSINO LUDOVICO",
+          "mese": "2024-07",
+          "turni": 1
+        },
+        {
+          "nome": "VERSINO LUDOVICO",
+          "mese": "2024-08",
+          "turni": 2
+        },
+        {
+          "nome": "VERSINO LUDOVICO",
+          "mese": "2024-09",
+          "turni": 3
+        },
+        {
+          "nome": "VIVIO MARCO",
+          "mese": "2024-01",
+          "turni": 2
+        },
+        {
+          "nome": "WARD MONICA",
+          "mese": "2024-03",
+          "turni": 4
+        },
+        {
+          "nome": "WARD MONICA",
+          "mese": "2024-04",
+          "turni": 14
+        },
+        {
+          "nome": "WARD MONICA",
+          "mese": "2024-05",
+          "turni": 9
+        },
+        {
+          "nome": "WARD MONICA",
+          "mese": "2024-06",
+          "turni": 6
+        },
+        {
+          "nome": "WARD MONICA",
+          "mese": "2024-07",
+          "turni": 4
+        }      
   
 ];
 
